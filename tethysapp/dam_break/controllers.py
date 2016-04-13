e1692942397f8a16014e3ab32cdee1067d355e6a
Alan D. Snow <alan.d.snow@usace.army.mil>
2016-04-13 10:59:12 -0500

    Updated tethys_sdk import

--- tethysapp/dam_break/controllers.py
+++ tethysapp/dam_break/controllers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from tethys_apps.sdk.gizmos import *
+from tethys_sdk.gizmos import *
 
 from .utilities import generate_flood_hydrograph, write_hydrograph_input_file
 
